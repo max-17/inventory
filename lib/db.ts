a2c6f5856667862fe5d7095be38e1a22ed36b89d
max-17 <max.1724dn@gmail.com>
2026-04-17 15:30:52 +0500

    fix: update import path for PrismaClient and adjust turbopack root configuration

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,6 @@
 import { PrismaNeon } from "@prisma/adapter-neon";
 import { neonConfig } from "@neondatabase/serverless";
-import { PrismaClient } from "./generated/prisma/client";
+import { PrismaClient } from "@/lib/generated/prisma/client";
 
 /**
  * Prisma 7 + Neon + Vercel (HTTP/Fetch Mode)
--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   turbopack: {
-    root: "/home/max/Desktop/projeects/inventory",
+    root: process.cwd(),
   },
 };
 
